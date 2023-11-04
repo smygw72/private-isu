b7ab0a1623581a47b4cd08e0c6a5a06bd02acb58
smygw72 <smygw72@preferred.jp>
2023-11-04 22:13:52 +0900

    fix

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -577,7 +577,13 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 	var posts []Post
 	err := getStructFromMemcache(mc, key, &posts)
 	if err != nil {
-		posts, err = makePosts(posts, getCSRFToken(r), false)
+		results := []Post{}
+		err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		posts, err = makePosts(results, getCSRFToken(r), false)
 		if err != nil {
 			log.Print(err)
 			return
